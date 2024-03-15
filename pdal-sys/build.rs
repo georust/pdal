d335d783f0f2633277fa60359766f3b30ae03f9f
Simeon H.K. Fitch <simeon.fitch@nuview.space>
2024-03-15 14:22:52 -0400

    License and copyright.

--- pdal-sys/build.rs
+++ pdal-sys/build.rs
@@ -1,3 +1,22 @@
+// MIT License
+//
+// Copyright (c) 2024 NUVIEW, Inc. <simeon.fitch@nuview.space>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+// OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 use std::env;
 use std::error::Error;
 use std::path::{Path, PathBuf};
@@ -7,8 +26,8 @@ use std::process::Command;
 // and https://github.com/rust-lang/cargo/issues/5077
 // for inspiration.
 fn main() -> Result<(), Box<dyn Error>> {
-    std::env::set_var("RUST_LOG", "bindgen=info");
-    let _ = env_logger::builder().is_test(true).try_init()?;
+    // std::env::set_var("RUST_LOG", "bindgen=info");
+    // let _ = env_logger::builder().is_test(true).try_init()?;
 
     let here = Path::new(env!("CARGO_MANIFEST_DIR"));
 
